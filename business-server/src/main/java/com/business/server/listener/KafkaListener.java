b6ee8a19ef0f5548552e927820b07563c9157aa9
Serendipity <jacksparrow414@163.com>
2023-11-13 20:17:26 +0800

    refactor: simplify MessageProducer and KafkaListener

--- business-server/src/main/java/com/business/server/listener/KafkaListener.java
+++ business-server/src/main/java/com/business/server/listener/KafkaListener.java
@@ -3,12 +3,14 @@ package com.business.server.listener;
 import com.business.server.producer.MessageProducer;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Vector;
 import org.apache.kafka.clients.producer.KafkaProducer;
 
 public class KafkaListener implements ServletContextListener {
 
-    private static final Vector<KafkaProducer> KAFKA_PRODUCERS = new Vector<>();
+    private static final List<KafkaProducer> KAFKA_PRODUCERS = new LinkedList<>();
     
     public static final Vector<Thread> KAFKA_CONSUMERS = new Vector<>();
 
